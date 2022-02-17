b697bb0be410b8a1823f34d784e794820fb3fa0e
RobertJG17 <robertjguerra@yahoo.com>
2022-02-17 12:42:38 -0800

    Initial commit for new repo

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Book from './Book';
+import './App.css';
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Book />
+    </div>
+  );
+}
+
+export default App;
